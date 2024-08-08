e7a74b90cf0129cb905bf92b9f9922273aff807b
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-08-09 01:46:24 +0300

    add getAllStores and getAllNearStores controllers

---
+++ controllers/storesControllers.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcrypt";
+
+import { httpError } from "../helpers/httpError.js";
+import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
+import { listNearStores, listStores } from "../services/storesServices.js";
+
+const getAllStores = async (_, res) => {
+  const result = await listStores();
+
+  res.json(result);
+};
+
+const getAllNearStores = async (_, res) => {
+  const result = await listNearStores();
+
+  res.json(result);
+};
+
+export default {
+  getAllStores: ctrlWrapper(getAllStores),
+  getAllNearStores: ctrlWrapper(getAllNearStores),
+};
